0ce5faac588cae5823676d520ca13c29e899c952
HandreMelo <34012482+HandreMelo@users.noreply.github.com>
2019-12-30 20:38:50 -0300

    Update salvar.php
    Atualização dos comandos sql

--- salvar.php
+++ salvar.php
@@ -4,7 +4,7 @@
     $email = $_POST['email'];
     $senha = $_POST['senha'];
     $sql = "INSERT INTO usuario (NOME, EMAIL, SENHA) VALUES ('$nome', '$email', '$senha')";
-    mysql_query($sql) or die(error());
+    $con -> query($sql) or die(error());
     $response = array("success" => true);
     echo json_encode($response);
 ?>
